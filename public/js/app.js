5a15c2cf90a2eaae5b468e4bb9c9caee0a3f2a16
Cristian Javier Azulay <cjadeveloper@gmail.com>
2020-11-13 19:10:01 -0300

    arq code refactor

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+btn.addEventListener('click', () => sendToPython());
+
+btn.dispatchEvent(new Event('click'));
+
+
 function sendToPython() {
     const { PythonShell } = require('python-shell');
 
@@ -5,18 +10,16 @@ function sendToPython() {
 
     let options = {
         mode: 'text',
+        scriptPath: 'server',
         args: [input.value]
     };
 
-    PythonShell.run('./calc.py', options, (err, results) => {
+    PythonShell.run('calc.py', options, (err, results) => {
         if (err) throw err;
         // result is an array consisting of msgs collected during execution
         console.log(results[0]);
 
+        // replace text 'result:' from array to ''
         result.innerText = results[0].replace('result: ', '');
     });
 }
-
-btn.addEventListener('click', () => sendToPython());
-
-btn.dispatchEvent(new Event('click'));
